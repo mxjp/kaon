57a338d84b87f75656ecafe9c06dd28372218bb3
Max J. Polster <max.polster@id-p.de>
2025-02-20 22:04:03 +0100

    Add basic context api

--- kaon.d.ts
+++ kaon.d.ts
@@ -18,3 +18,70 @@ export function teardown(hook: TeardownHook): void;
  * @returns A function to call all registered hooks in reverse order.
  */
 export function capture<F extends Fn>(fn: F, ...args: Parameters<F>): TeardownHook;
+
+export type ContextState<T> = [Context<T>, T];
+
+/**
+ * Allows passing arbitrary values down the synchronous call stack.
+ *
+ * @example
+ * ```js
+ * const MESSAGE = new Context();
+ *
+ * MESSAGE.inject("Hello World!", () => {
+ *   console.log(MESSAGE.get());
+ * });
+ * ```
+ */
+export class Context<T> {
+	constructor(defaultValue: T);
+	constructor(...defaultValue: T extends undefined ? [] : [T]);
+
+	/**
+	 * Get the current value of this context.
+	 */
+	get(): T;
+
+	/**
+	 * Create a state from this context with a value.
+	 *
+	 * @example
+	 * ```js
+	 * Context.inject([
+	 *   CONTEXT_A.with(42),
+	 *   CONTEXT_B.with(77),
+	 * ], () => {
+	 *   ...
+	 * });
+	 * ```
+	 */
+	with(value: T): ContextState<T>;
+
+	/**
+	 * Run a function while injecting a value in this context.
+	 *
+	 * @param value The value to inject.
+	 * @param fn The function to run.
+	 */
+	inject<F extends Fn>(value: T | null | undefined, fn: F, ...args: Parameters<F>): ReturnType<F>;
+
+	/**
+	 * Run a function in a new context window without any injected value.
+	 */
+	static window<F extends Fn>(fn: F, ...args: Parameters<F>): ReturnType<F>;
+
+	/**
+	 * Run a function while injecting into multiple contexts.
+	 */
+	static inject<F extends Fn>(states: ContextState<unknown>[], fn: F, ...args: Parameters<F>): ReturnType<F>;
+
+	/**
+	 * Capture all current context states.
+	 */
+	static capture(): ContextState<unknown>[];
+
+	/**
+	 * Wrap a function to always run in the current context.
+	 */
+	static wrap<F extends Fn>(fn: F): F;
+}
--- kaon.js
+++ kaon.js
@@ -1,5 +1,6 @@
 
 const _LIFECYCLE = [];
+const _CONTEXT = [[]];
 
 const _in = (stack, frame, fn, ...args) => {
 	try {
@@ -28,3 +29,55 @@ export const capture = (fn, ...args) => {
 	}
 	return () => _dispose(hooks);
 };
+
+export class Context {
+	#stack = [];
+	#window = 0;
+
+	constructor(value) {
+		this.default = value;
+	}
+
+	get() {
+		return this.#window === _CONTEXT.length
+			? _head(this.#stack) ?? this.default
+			: this.default;
+	}
+
+	with(value) {
+		return [this, value];
+	}
+
+	inject(value, fn, ...args) {
+		const window = _head(_CONTEXT);
+		const parent = this.#window;
+		const stack = this.#stack;
+		try {
+			this.#window = _CONTEXT.length;
+			window.push(this);
+			stack.push(value);
+			return fn(...args);
+		} finally {
+			stack.pop();
+			window.pop();
+			this.#window = parent;
+		}
+	}
+
+	static window(fn, ...args) {
+		return _in(_CONTEXT, [], fn, ...args);
+	}
+
+	static inject([state, ...rest], fn, ...args) {
+		return state ? state[0].inject(state[1], Context.inject, rest, fn, ...args) : fn(...args);
+	}
+
+	static capture() {
+		return _head(_CONTEXT).map(c => [c, c.get()]);
+	}
+
+	static wrap(fn) {
+		const states = Context.capture();
+		return (...args) => Context.window(Context.inject, states, fn, ...args);
+	}
+}
