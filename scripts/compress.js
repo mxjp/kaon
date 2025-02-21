01f0f16001c9da8a8833ab6452aaa39805c69093
Max J. Polster <max.polster@id-p.de>
2025-02-21 21:14:29 +0100

    Enable srouce maps

--- scripts/compress.js
+++ scripts/compress.js
@@ -6,5 +6,10 @@ import { brotliCompressSync, gzipSync } from "node:zlib";
 const root = join(fileURLToPath(import.meta.url), "../..");
 const minified = await readFile(join(root, "kaon.min.js"));
 
-await writeFile(join(root, "kaon.js.br"), brotliCompressSync(minified));
-await writeFile(join(root, "kaon.js.gz"), gzipSync(minified));
+const brotli = brotliCompressSync(minified);
+await writeFile(join(root, "kaon.min.js.br"), brotli);
+console.log(`brotli: ${brotli.length}b`);
+
+const gzip = gzipSync(minified);
+await writeFile(join(root, "kaon.min.js.gz"), gzip);
+console.log(`gzip: ${gzip.length}b`);
